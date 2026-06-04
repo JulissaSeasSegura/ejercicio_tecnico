fd163db677879647caa632930c47d182d13ef39f
unknown <yulissaseas98@gmail.com>
2026-06-03 23:03:01 -0600

    Conexión a base de datos H2

--- Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/model/Cliente.java
+++ Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/model/Cliente.java
@@ -1,20 +1,21 @@
 package com.julissa.ejercicio_tecnico.model;
 
 import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Cliente {
 
-    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nombre, correo, usuarioCreacion;
     private LocalDateTime fechaCreacion;
 
-    //Constructor
-    public Cliente(long id, String nombre, String correo, String usuarioCreacion, LocalDateTime fechaCreacion){
-        this.id = id;
-        this.nombre = nombre;
-        this.correo = correo;
-        this.usuarioCreacion = usuarioCreacion;
-        this.fechaCreacion = fechaCreacion;
+    public Cliente() {
     }
+
 }
