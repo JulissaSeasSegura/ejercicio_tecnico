c93e3f02c874a840f07bbbd9e1ce812672126e7b
unknown <yulissaseas98@gmail.com>
2026-06-04 11:02:29 -0600

    Anotaciones

--- Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/controller/ClienteController.java
+++ Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/controller/ClienteController.java
@@ -12,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Tag(name = "Clientes", description = "Operaciones relacionadas con la gestión de clientes.")
 public class ClienteController {
 
     private final ClienteService clienteService;
@@ -23,16 +26,19 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    @Operation(summary = "Listar clientes", description = "Obtiene todos los clientes registrados.")
     @GetMapping
     public List<Cliente> getClientes() {
         return clienteService.listarClientes();
     }
 
+    @Operation(summary = "Buscar el cliente por ID", description = "Obtiene un cliente registrado mediante su identificador.")
     @GetMapping("/{id}")
     public Optional<Cliente> getCliente(@PathVariable Long id) {
         return clienteService.buscarClientePorId(id);
     }
 
+    @Operation(summary = "Crear cliente", description = "Registra un nuevo cliente.")
     @PostMapping
     public Cliente postCliente(@Valid @RequestBody Cliente cliente) {
         return clienteService.crearCliente(cliente);
