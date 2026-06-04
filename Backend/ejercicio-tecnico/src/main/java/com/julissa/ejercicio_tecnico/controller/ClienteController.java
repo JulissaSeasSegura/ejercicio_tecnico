c9011a1917da3b37d02637721d8c125eda80199b
unknown <yulissaseas98@gmail.com>
2026-06-03 23:32:13 -0600

    Creación de Endpoints

--- Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/controller/ClienteController.java
+++ Backend/ejercicio-tecnico/src/main/java/com/julissa/ejercicio_tecnico/controller/ClienteController.java
@@ -1,34 +1,42 @@
 package com.julissa.ejercicio_tecnico.controller;
 
+import com.julissa.ejercicio_tecnico.model.Cliente;
+import com.julissa.ejercicio_tecnico.service.ClienteService;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
     //Lista todos los clientes registrados
     @GetMapping
-    public String getClientes(@RequestParam String param) {
-        return new String();
+    public List<Cliente> getClientes() {
+        return clienteService.listarClientes();
     }
 
     //Consulta un cliente por su identificador
     @GetMapping("/{id}")
-    public String getCliente(@RequestParam String param) {
-        return new String();
+    public Optional<Cliente> getCliente(@PathVariable Long id) {
+        return clienteService.buscarClientePorId(id);
     }
-    
+
     //Crea un nuevo cliente
     @PostMapping
-    public String postCliente(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    public Cliente postCliente(@RequestBody Cliente cliente) {
+        return clienteService.guardarCliente(cliente);
     }
-    
+
 }
